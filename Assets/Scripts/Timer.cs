9996893cd2a95d5e89739f6da2760b07c8620e4b
Jlim <jimmyetlim@gmail.com>
2021-10-30 01:13:47 -0400

    Ok Timer et temtative Fix Teleportation

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private float time { get; set; }
+    private bool timerStop { get; set; }
+
+    [SerializeField]
+    private TextMesh text; 
+
+    private Coroutine timerRoutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+        Coroutine timerRoutine = StartCoroutine("TimerExecute");
+    }
+
+	private void FixedUpdate()
+	{
+
+
+    }
+
+	public void Stop()
+	{
+        timerStop = true;
+        StopCoroutine(timerRoutine);
+
+    }
+
+	IEnumerator TimerExecute()
+	{
+        while (true) 
+        {
+            if (!timerStop)
+            {
+                time += 0.1f;
+                if (text != null)
+                    text.text = ""+ (Mathf.Round(time * 10) * 0.1f).ToString("F1");
+                yield return new WaitForSeconds(.1f);
+            }
+        }
+       
+	}
+}
