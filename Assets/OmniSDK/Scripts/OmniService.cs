2ead1c9ca50a3f7b5640079b18be953f8a6b39d6
Jlim <jimmyetlim@gmail.com>
2021-11-18 01:46:14 -0500

    Init Config

---
+++ Assets/OmniSDK/Scripts/OmniService.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class OmniService 
+{
+    public static bool isPresent()
+    {
+        var xrDisplaySubsystems = new List<XRDisplaySubsystem>();
+        SubsystemManager.GetInstances<XRDisplaySubsystem>(xrDisplaySubsystems);
+        foreach (var xrDisplay in xrDisplaySubsystems)
+        {
+            if (xrDisplay.running)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
